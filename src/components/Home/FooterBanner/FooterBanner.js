74885b86dcbeef66c4947c219140b1387ccba2ac
Kawsar Hossain <86672839+thekawsarhossain@users.noreply.github.com>
2022-01-05 01:05:38 +0600

    Revert "online courses added"

---
+++ src/components/Home/FooterBanner/FooterBanner.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import "./FooterBanner.css";
+
+const FooterBanner = () => {
+  return (
+  <div>
+      
+  </div>);
+};
+
+export default FooterBanner;
---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CourseSlider from "../CourseSlider/CourseSlider";
+import PopularCourse from "../PopularCourse/PopularCourse";
+
+const Home = () => {
+  return <div>
+   
+    <PopularCourse/>
+   <CourseSlider/>
+   
+  </div>;
+};
+
+export default Home;
