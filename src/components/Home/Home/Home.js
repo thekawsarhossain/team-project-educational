dde280561aac8ef9ad36f75b7f693692bf7ef309
Rakibul santo <SANTODON176@gmail.com>
2022-01-05 03:10:03 +0600

    add pricing card

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import CourseSlider from "../CourseSlider/CourseSlider";
 import OnlineCourse from "../OnlineCourse/OnlineCourse";
 import PopularCourse from "../PopularCourse/PopularCourse";
+import Pricing from "../Pricing/Pricing";
 
 const Home = () => {
   return <div>
     <PopularCourse />
     <CourseSlider />
     <OnlineCourse />
+    <Pricing/>
   </div>;
 };
 
