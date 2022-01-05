cddea62754d1cf477bfba9120eb5b519797eba03
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-05 12:12:42 +0600

    navigation part added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,18 +1,23 @@
+import { Box } from "@mui/system";
 import React from "react";
+import Navigation from "../../Shared/Navigation/Navigation";
 import CourseSlider from "../CourseSlider/CourseSlider";
 import Event from "../Event/Event";
 import OnlineCourse from "../OnlineCourse/OnlineCourse";
 import PopularCourse from "../PopularCourse/PopularCourse";
 import Pricing from "../Pricing/Pricing";
+import Footer from '../../Shared/Footer/Footer';
 
 const Home = () => {
-  return <div>
+  return <Box>
+    <Navigation />
     <PopularCourse />
     <CourseSlider />
     <OnlineCourse />
     <Event />
     <Pricing />
-  </div>;
+    <Footer />
+  </Box>;
 };
 
 export default Home;
