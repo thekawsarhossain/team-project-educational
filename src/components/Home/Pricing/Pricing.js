0f4965848b81dbf37130ad645d574382156b4c23
Rakibul santo <SANTODON176@gmail.com>
2022-01-05 15:04:26 +0600

    adds

--- src/components/Home/Pricing/Pricing.js
+++ src/components/Home/Pricing/Pricing.js
@@ -1,6 +1,6 @@
 import { Container } from "@mui/material";
 import React from "react";
-import {BrowserRouter as Router,  NavLink, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Annual from "./Annual";
 import Monthly from "./Monthly";
 
@@ -12,29 +12,18 @@ const Pricing = () => {
     <div className="pricing-section">
       <div className="price-intro">
       <h1>Simple <br />
-          All Inclusive Pricing</h1> 
+          All Inclusive Pricing</h1>
           <p>No contracts. No surprise fees.</p>
       </div>
       <div className="pricing-card">
         <Router>
           <div className="price-button">
-          <NavLink
-              to="/monthly"
-              style={isActive => ({
-                color: isActive ? "white" : "black",
-                background: isActive ? "#2B4EFF" : 'white',
-              })} >
-           <span className="monthly-btn"> Monthly Plan</span>
-          </NavLink>
-          <NavLink
-              to="/annual"
-              style={isActive => ({
-                color: isActive ? "white" : "black",
-                background: isActive ? "#2B4EFF" : 'white',
-              })} >
-            <span className="annual-btn">Annual Plan</span>
-          </NavLink>
-         
+            <Link to='/monthly'>
+            <button>Monthly Plan</button>
+            </Link>
+           <Link to='/annual'>
+           <button>Annual Plan</button>
+           </Link>
           </div>
           <Switch>
             <Route exact path='/'>
