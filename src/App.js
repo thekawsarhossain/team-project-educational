6f8fa6add35db39376ae75cfa8bf3c630a669ab6
Rakibul santo <SANTODON176@gmail.com>
2022-01-06 22:00:22 +0600

    error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router,  Switch,Route } from 'react-router-dom';
 import './App.css';
 import Blogs from './components/Blogs/Blogs';
 import Home from './components/Home/Home/Home';
