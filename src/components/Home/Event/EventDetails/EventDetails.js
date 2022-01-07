c6996563a6ee9be62ee8655f165b2b7febcbba23
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-07 10:46:35 +0600

    email js st added

--- src/components/Home/Event/EventDetails/EventDetails.js
+++ src/components/Home/Event/EventDetails/EventDetails.js
@@ -4,12 +4,13 @@ import { Container, Box, Grid } from '@mui/material'
 import { Link } from 'react-router-dom';
 const EventDetails = () => {
 
-    const eventDetails = useSelector(state => state.events.eventDetails);
-    console.log(eventDetails)
+  const eventDetails = useSelector(state => state.events.eventDetails);
+  const status = useSelector(state => state.events.status);
+  console.log(status)
 
-    return (
-        <div>
-           <Container>
+  return (
+    <div>
+      <Container>
         <Box sx={{ width: '100%' }}>
           <Grid container
             rowSpacing={3}
@@ -42,21 +43,21 @@ const EventDetails = () => {
                   <img src={eventDetails.img} alt="" />
                   <h1>Description</h1>
                   <p>He legged it up the kyver have it mush super me old mucker cheeky naff that are you taking the piss, blow off down the pub bite your arm off the wireless boot cor blimey guvnor happy days bender what a load of rubbish, say pardon me horse play spiffing Why car boot gosh bubble and squeak. Cheers Richard bugger show off show off pick your nose and blow off get stuffed mate chancer in my flat loo, bevvy amongst hunky-dory bender bubble and squeak me old mucker vagabond, barmy spend a penny chimney pot young delinquent bum bag the bee’s knees chap, gosh nice one knees up the wireless Charles such a fibber. Mush barmy bleeding Jeffrey pardon me barney grub loo cup of tea bubble and squeak bugger all mate say, I bloke matie boy tickety-boo
-                     give us a bell up the duff bugger lurgy wind up I don’t want no agro.</p>
+                    give us a bell up the duff bugger lurgy wind up I don’t want no agro.</p>
                 </div>
               </div>
             </Grid>
             <Grid item xs={12} md={4}>
               <div className='details-2'>
-                <h1>${eventDetails.newPrice}
-                  <span><del>{eventDetails.previousPrice}</del></span></h1>
-<li><i class="fas fa-hourglass-end"></i> End : <span>{eventDetails.time}</span></li>
-<li><i class="fas fa-hourglass-half"></i> Time : <span>{eventDetails.date}</span></li>
-<li><i class="fas fa-map-marker-alt"></i> Venue : <span>{eventDetails.place}</span></li>
+                {/* <h1>${eventDetails.newPrice}
+                  <span><del>{eventDetails.previousPrice}</del></span></h1> */}
+                <li><i class="fas fa-hourglass-end"></i> End : <span>{eventDetails.time}</span></li>
+                <li><i class="fas fa-hourglass-half"></i> Time : <span>{eventDetails.date}</span></li>
+                <li><i class="fas fa-map-marker-alt"></i> Venue : <span>{eventDetails.place}</span></li>
 
 
                 <Link to='/#'>
-                  <button style={{background:"#F2277E"}}>Enroll now <i class="fas fa-arrow-right" style={{color:"white"}}></i></button>
+                  <button style={{ background: "#F2277E" }}>Join <i class="fas fa-arrow-right" style={{ color: "white" }}></i></button>
                 </Link>
               </div>
             </Grid>
@@ -64,8 +65,8 @@ const EventDetails = () => {
           </Grid>
         </Box>
       </Container>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default EventDetails;
